0214ac75d6d5ec09674f10c89e72636d3289bdaf
pwarsrawp <pwarsrawp@gmail.com>
2023-07-12 17:45:10 +0200

    Models and seed.js

---
+++ models/Car.model.js
@@ -0,0 +1,23 @@
+const { Schema, model } = require("mongoose");
+
+const carSchema = new Schema(
+  {
+    brand: String,
+    model: String,
+    class: String,
+    gearbox: String,
+    fuel: String,
+    doors: Number,
+    pax: Number,
+    luggage: Number,
+    img: String,
+    ppd: Number,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Car = model("Car", carSchema);
+
+module.exports = Car;
