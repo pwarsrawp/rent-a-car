60ef95f4944720af86a8d014d9dc1824b83b2d77
nicolapasa <nicola.pasa@gmail.com>
2023-07-13 15:21:59 +0200

    car details

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const Car = require("../models/Car.model");
-
+const Branch = require("../models/Branch.model");
 
 
 /* GET home page */
@@ -21,6 +21,30 @@ router.get("/cars", async (req, res, next) => {
   }
   
 });
+//details car page
+router.get("/cars/:id", async (req, res, next) => {
+  try {
+    const car = await Car.findById(req.params.id);
+    const branches = await Branch.find();
+    res.render("car-details", {car, branches});
+  } catch(error) {
+    console.log("There was an error displaying all cars", error)
+  }
+  
+});
+//booking
+router.post("/book/:id", async (req, res, next) => {
 
+  console.log(req.params)
+  try {
+
+    const car = await Car.findById(req.params.id);
+   // res.render("car-details", {car});
+   res.send(req.body)
+  } catch(error) {
+    console.log("There was an error displaying all cars", error)
+  }
+  
+});
 
 module.exports = router;
