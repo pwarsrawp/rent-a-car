787107c3a3eacb39c9dfc8a50756a025aed18b1f
pwarsrawp <pwarsrawp@gmail.com>
2023-07-13 12:22:14 +0200

    what the hell

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const router = express.Router();
 
+const Car = require("../models/Car.model");
+
+
+
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("index");
 });
+
+// CAR PAGE
+router.get("/cars", async (req, res, next) => {
+  try {
+    const allCars = await Car.find();
+    res.render("cars", {allCars});
+    console.log(allCars)
+  } catch(error) {
+    console.log("There was an error displaying all cars", error)
+  }
+  
+});
+
 
 module.exports = router;
