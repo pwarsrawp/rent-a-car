828633369bcdce3c745ddf66ff0946554c021dc2
pwarsrawp <pwarsrawp@gmail.com>
2023-07-14 17:33:33 +0200

    floating menu wip

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,22 @@
+const menuButton = document.getElementById('menu-button')
+const menu = document.getElementById('menu')
+
 // https://developer.mozilla.org/en-US/docs/Web/API/Window/DOMContentLoaded_event
 document.addEventListener("DOMContentLoaded", () => {
   console.log("rent-a-car JS imported successfully!");
+});
+
+
+menuButton.addEventListener("click", () => {
+  if (menuButton.classList.contains("fa-bars")) {
+    menuButton.classList.remove("fa-bars");
+    menuButton.classList.add("fa-xmark");
+    menu.style.display = "flex"
+
+  } else {
+    menuButton.classList.add("fa-bars");
+    menuButton.classList.remove("fa-xmark");
+    menu.style.display = "none"
+    menu.appendChild("<li>aaaa</li>")
+  }  
 });
