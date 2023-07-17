f800ff8dab1b0be6d6ce3cf8eaa015dfa7846cee
pwarsrawp <pwarsrawp@gmail.com>
2023-07-17 17:55:11 +0200

    admin panel functionality done

--- config/cloudinary.config.js
+++ config/cloudinary.config.js
@@ -1,23 +1,23 @@
 // config/cloudinary.config.js
 
-const cloudinary = require('cloudinary').v2;
-const { CloudinaryStorage } = require('multer-storage-cloudinary');
-const multer = require('multer');
+const cloudinary = require("cloudinary").v2;
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const multer = require("multer");
 
-cloudinary.config({ 
-    cloud_name: 'devjy7hwv', 
-    api_key: '937658318424121', 
-    api_secret: 'ENH_Va8Nc-Uit1nF4C70dqakBlw' 
-  });
+cloudinary.config({
+  cloud_name: "dqqfa0c1d",
+  api_key: "229864381841766",
+  api_secret: "***************************",
+});
 
 const storage = new CloudinaryStorage({
   // cloudinary: cloudinary,
   cloudinary,
   params: {
-    allowed_formats: ['jpg', 'png', 'webp'],
-    folder: 'movie-project' // The name of the folder in cloudinary
+    allowed_formats: ["jpg", "png", "webp"],
+    folder: "movie-project", // The name of the folder in cloudinary
     // resource_type: 'raw' => this is in case you want to upload other type of files, not just images
-  }
+  },
 });
 
 //                     storage: storage
--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -6,107 +6,161 @@ const Car = require("../models/Car.model");
 const Branch = require("../models/Branch.model");
 const salt = bcrypt.genSaltSync(13);
 
-const { isLoggedOut, isLoggedIn, isAdmin } = require("../middleWares/route-protect.js");
+const {
+  isLoggedOut,
+  isLoggedIn,
+  isAdmin,
+} = require("../middleWares/route-protect.js");
 
-const fileUploader = require('../config/cloudinary.config');
+const fileUploader = require("../config/cloudinary.config");
 router.get("/admin", isLoggedIn, isAdmin, async (req, res, next) => {
-    const cars = await Car.find()
-    const users = await User.find()
-    const bookings = await Booking.find().populate('car').populate('userId')
-    const branches = await Branch.find()
-    bookings.forEach(async booking => {
-      let date1=new Date(booking.endDate)
-      let date2=new Date(booking.startDate)
-      booking.endDate= date1.getDate() + ' ' + date1.toLocaleString("en-US",{month: 'short'}) + ' ' + date1.getFullYear()
-      booking.startDate= date2.getDate() + ' ' + date2.toLocaleString("en-US",{month: 'short'}) + ' ' + date2.getFullYear()
-      booking.total=booking.total.toFixed(2)
-
-    });
-    
-    
-   
-   
-    res.render("admin/admin-profile", {bookings, users, branches, cars, userid: req.session.currentUser});
+  const cars = await Car.find();
+  const users = await User.find();
+  const bookings = await Booking.find().populate("car").populate("userId");
+  const branches = await Branch.find();
+  bookings.forEach(async (booking) => {
+    let date1 = new Date(booking.endDate);
+    let date2 = new Date(booking.startDate);
+    booking.endDate =
+      date1.getDate() +
+      " " +
+      date1.toLocaleString("en-US", { month: "short" }) +
+      " " +
+      date1.getFullYear();
+    booking.startDate =
+      date2.getDate() +
+      " " +
+      date2.toLocaleString("en-US", { month: "short" }) +
+      " " +
+      date2.getFullYear();
+    booking.total = booking.total.toFixed(2);
   });
-//new branch form
-  router.get("/branch", isLoggedIn, isAdmin, async (req, res, next) => {
-    
-    res.render('admin/newBranch')
-  
-  })
-  router.post("/branch", async (req, res, next) => {
-    
 
-    Branch.create(req.body)
+  res.render("admin/admin-profile", {
+    bookings,
+    users,
+    branches,
+    cars,
+    userid: req.session.currentUser,
+  });
+});
 
-    res.redirect('/admin')
-  
-  })
-  router.get("/branch/:id/edit", isLoggedIn, isAdmin, async (req, res, next) => {
+// BRANCH NEW
+router.get("/branch", isLoggedIn, isAdmin, async (req, res, next) => {
+  res.render("admin/newBranch");
+});
 
-    const branch=await Branch.findById(req.params.id)  
-    
-    res.render('admin/editBranch', {branch})
-  
-  })
-  router.post("/branch/:id/edit", async (req, res, next) => {
+router.post("/branch", async (req, res, next) => {
+  Branch.create(req.body);
 
-    await Branch.findByIdAndUpdate(req.params.id, req.body)  
-    
-    res.redirect('/admin')
-  
-  })
-  router.post("/branch/:id/delete", isLoggedIn, isAdmin, async (req, res, next) => {
-    
-   await Branch.findByIdAndDelete(req.params.id)  
+  res.redirect("/admin");
+});
+// BRANCH EDIT
+router.get("/branch/:id/edit", isLoggedIn, isAdmin, async (req, res, next) => {
+  const branch = await Branch.findById(req.params.id);
 
-    res.redirect('/admin')
-  
-  })
+  res.render("admin/editBranch", { branch });
+});
 
-  /* make a new car */
-  router.get("/car", isLoggedIn, isAdmin, async (req, res, next) => {
-    
-    res.render('admin/newCar', {userid: req.session.currentUser})
-  
-  })
-  router.post("/car", fileUploader.single('img'), async (req, res, next) => {
-    
-    const payload={...req.body}
-    payload.img=req.file.path 
-    Car.create(payload)
+router.post("/branch/:id/edit", async (req, res, next) => {
+  await Branch.findByIdAndUpdate(req.params.id, req.body);
 
-    res.redirect('/admin')
-  
-  })
-  router.get("/car/:id/edit", isLoggedIn, isAdmin, async (req, res, next) => {
+  res.redirect("/admin");
+});
+// BRANCH DELETE
+router.post(
+  "/branch/:id/delete",
+  isLoggedIn,
+  isAdmin,
+  async (req, res, next) => {
+    await Branch.findByIdAndDelete(req.params.id);
 
-    const car=await Car.findById(req.params.id)  
-    
-    res.render('admin/editCar', {car})
-  
-  })
-  router.post("/car/:id/edit", fileUploader.single('img'), async (req, res, next) => {
+    res.redirect("/admin");
+  }
+);
 
-    const payload={...req.body}
-    if(typeof req.file != 'undefined') {
-      payload.img=req.file.path 
-    } else{
-    delete payload.img
+// CAR NEW
+router.get("/car", isLoggedIn, isAdmin, async (req, res, next) => {
+  res.render("admin/newCar", { userid: req.session.currentUser });
+});
+
+router.post("/car", fileUploader.single("img"), async (req, res, next) => {
+  const payload = { ...req.body };
+  payload.img = req.file.path;
+  Car.create(payload);
+
+  res.redirect("/admin");
+});
+// CAR EDIT
+router.get("/car/:id/edit", isLoggedIn, isAdmin, async (req, res, next) => {
+  const car = await Car.findById(req.params.id);
+
+  res.render("admin/editCar", { car });
+});
+
+router.post(
+  "/car/:id/edit",
+  fileUploader.single("img"),
+  async (req, res, next) => {
+    const payload = { ...req.body };
+    if (typeof req.file != "undefined") {
+      payload.img = req.file.path;
+    } else {
+      delete payload.img;
     }
-      
-    await Car.findByIdAndUpdate(req.params.id, payload)
-    
-   
-    res.redirect('/admin')
-  
-  })
 
-  router.post("/car/:id/delete",  async (req, res, next) => {
-    
-    await Car.findByIdAndDelete(req.params.id)  
- 
-     res.redirect('/admin')
-   
-   })
-  module.exports = router;
+    await Car.findByIdAndUpdate(req.params.id, payload);
+
+    res.redirect("/admin");
+  }
+);
+// CAR DELETE
+router.post("/car/:id/delete", async (req, res, next) => {
+  await Car.findByIdAndDelete(req.params.id);
+
+  res.redirect("/admin");
+});
+
+// USER NEW
+router.get("/user", isLoggedIn, isAdmin, async (req, res, next) => {
+  res.render("admin/newUser", { userid: req.session.currentUser });
+});
+
+router.post("/user", async (req, res, next) => {
+  tempUser = { ...req.body };
+  delete tempUser.password;
+  tempUser.passwordHash = bcrypt.hashSync(req.body.password, salt);
+  try {
+    const newUser = await User.create(tempUser);
+    res.redirect("/admin");
+  } catch (error) {
+    console.log("Error creating new user", error);
+  }
+});
+// USER EDIT
+router.get("/user/:id/edit", isLoggedIn, isAdmin, async (req, res, next) => {
+  const user = await User.findById(req.params.id);
+
+  res.render("admin/editUser", { user });
+});
+
+router.post("/user/:id/edit", async (req, res, next) => {
+  tempUser = { ...req.body };
+  if (typeof req.body.newPassword !== "undefined") {
+    delete tempUser.password;
+    tempUser.passwordHash = bcrypt.hashSync(req.body.newPassword, salt);
+    await User.findByIdAndUpdate(req.params.id, tempUser);
+  } else {
+    delete tempUser.password;
+    await User.findByIdAndUpdate(req.params.id, req.body);
+  }
+  res.redirect("/admin");
+});
+// USER DELETE
+router.post("/user/:id/delete", async (req, res, next) => {
+  await User.findByIdAndDelete(req.params.id);
+
+  res.redirect("/admin");
+});
+
+module.exports = router;
